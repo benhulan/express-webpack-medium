146d744334afb2d0f14496559333b5ba64b2d487
Ben Hulan <ben.hulan@bounteous.com>
2020-03-10 13:17:32 -0500

    Has ESLint code linting

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   entry: {
     main: ['webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000', './src/index.js']
@@ -15,6 +16,17 @@ module.exports = {
   devtool: 'source-map',
   module: {
     rules: [
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader",
+        options: {
+          emitWarning: true,
+          failOnError: false,
+          failOnWarning: false
+        }
+      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
